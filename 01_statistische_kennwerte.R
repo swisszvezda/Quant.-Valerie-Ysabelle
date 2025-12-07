199a13446a1f54c4adf2a94f5b480fe2bdc5c2a0
valerieany <valerie.schneider@stud.fhgr.ch>
2025-12-07 15:20:18 +0100

    Convert Q6.1.2_1 to numeric and compute descriptive statistics (mean, median, variance, sd, mode)

--- 01_statistische_kennwerte.R
+++ 01_statistische_kennwerte.R
@@ -13,3 +13,24 @@ get_mode(guess$Q2.1_1)
 var(guess$Q2.1_1, na.rm = TRUE)
 
 sd(guess$Q2.1_1, na.rm = TRUE)
+
+
+#BERECHNUNG VON ZWEITER VARIABEL
+
+#na.rm = TRUE ->entfernt fehlende werde, NA remove
+str(guess$Q6.1.2_1) #str findet heraus, was die Variabel ist 
+guess$Q6.1.2_1 <- as.numeric(guess$Q6.1.2_1) #Werte von Charakteren in Zahlen wandeln
+
+
+median(guess$Q6.1.2_1, na.rm = TRUE)
+mean(guess$Q6.1.2_1, na.rm = TRUE)
+var(guess$Q6.1.2_1, na.rm = TRUE)
+sd(guess$Q6.1.2_1, na.rm = TRUE)
+
+
+modus <- names(sort(table(guess$Q6.1.2_1), decreasing = TRUE))[1]
+modus
+#MODUS ERKLÄRUNG:
+table(guess$Q6.1.2_1) #zählt Häufigkeiten (auflistung aller Werte und die Häufigkeit darunter)
+#sort(..., decreasing=TRUE) -> sortiert nach grösster Häufigkeit
+#names(...)[1] -> liefert die Kategorie mit der höchsten Häufigkeit
