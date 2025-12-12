89d6c3b92590265c3430a3ceca52d160d356c592
valerieany <valerie.schneider@stud.fhgr.ch>
2025-12-12 14:54:40 +0100

    added boxplot to the right of histogram and added a main titel

--- 02_graphs.R
+++ 02_graphs.R
@@ -1,3 +1,6 @@
+# 1 Zeile, 2 Spalten
+par(mfrow = c(1, 2), oma = c(0,0,3,0))
+
 h <- hist(
   guess$Q6.1.2_1,
   breaks = seq(0.5, 7.5, by = 1),   # sorgt für saubere Balken für 1,2,3,...7
@@ -5,7 +8,7 @@ h <- hist(
   border = "black",
   xlab = "Skala von 1-7 (1 = question, 7 = obey)",
   ylab = "Anzahl Personen",
-  main = "Q6.1.2_1 - In my society followers are expected to"
+  main = ""
 )
 
 # Prozente berechnen
@@ -18,4 +21,16 @@ text(
   pos = 1
 )
 
+boxplot(
+  guess$Q6.1.2_1,
+  vertical = TRUE,               # horizontale Box besser neben Histogramm
+  col = "lightgreen",
+  #main = "Boxplot",
+  xlab = "Skala von 1-7 (1 = question, 7 = obey)"
+)
+
+mtext("Q6.1.2_1 - In my society, followers are expected to", 
+      outer = TRUE,   # für die gesamte Plotfläche
+      cex = 1.5,      # Schriftgröße
+      font = 2)       # Fettschrift
 
