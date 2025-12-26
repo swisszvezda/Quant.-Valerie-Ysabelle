af19fbc4b88960476ae8ccb4a02bd955c2d8fe33
Ysabelle Moser <swissmoser@hotmail.com>
2025-12-26 10:40:18 +0100

    Projekt Kennwerte finalisieren + Projektbericht aufsetzen

--- 01_statistische_kennwerte.R
+++ 01_statistische_kennwerte.R
@@ -34,3 +34,59 @@ modus
 table(guess$Q6.1.2_1) #zählt Häufigkeiten (auflistung aller Werte und die Häufigkeit darunter)
 #sort(..., decreasing=TRUE) -> sortiert nach grösster Häufigkeit
 #names(...)[1] -> liefert die Kategorie mit der höchsten Häufigkeit
+
+
+#START PROJEKT
+
+#ERSTER SCHRITT FÜR ALLE VARIABLEN -> ÜBERSICHT + SKALA BILDEN
+
+#Gründungsabsicht, Variable 1
+guess$Q4.1.1_2 <- as.numeric(guess$Q4.1.1_2) #Variablenwerte in Zahlen umwandeln
+summary(guess$Q4.1.1_2, na.rm = TRUE) #Überblick Werte der Variable // na.rm = TRUE = fehlende Werte werden ignoriert
+
+#Erwartete Reaktion des persönlichen Umfelds, Variable 2
+#Als erstes: Alle drei Variablen gleichzeitig in Zahlen umwandeln
+guess[, c("Q6.2_1", "Q6.2_2", "Q6.2_3")] <-
+  lapply(guess[, c("Q6.2_1", "Q6.2_2", "Q6.2_3")], as.numeric)
+#Als zweites: Skala bilden (Mittelwert der drei Items) -> neue Variable "umfeldreaktion"
+#"rowMeans" = Mittelwert pro Person
+guess$umfeldreaktion <- rowMeans(
+  guess[, c("Q6.2_1", "Q6.2_2", "Q6.2_3")],
+  na.rm = TRUE
+)
+#Als drittes: Überblick Werte der neuen Variable "umfeldreaktion"
+summary(guess$umfeldreaktion)
+
+#Wahrnehmung gesellschaftlicher Strukturen, Variable 3
+#Als erstes: Alle drei Variablen gleichzeitig in Zahlen umwandeln
+guess[, c("Q6.1.1_1", "Q6.1.2_1", "Q6.1.3_1")] <-
+  lapply(guess[, c("Q6.1.1_1", "Q6.1.2_1", "Q6.1.3_1")], as.numeric)
+#Als zweites: Skala bilden (Mittelwert der drei Items) -> neue Variable "umfeldreaktion"
+#"rowMeans" = Mittelwert pro Person
+guess$gesellschaft <- rowMeans(
+  guess[, c("Q6.1.1_1", "Q6.1.2_1", "Q6.1.3_1")],
+  na.rm = TRUE
+)
+#Als drittes: Überblick Werte der neuen Variable "umfeldreaktion"
+summary(guess$gesellschaft)
+
+#ZWEITER SCHRITT FÜR ALLE DREI VARIABLEN -> BERECHNUNG MITTELWERT + STANDARDABWEICHUNG
+mean(guess$Q4.1.1_2, na.rm = TRUE)
+sd(guess$Q4.1.1_2, na.rm = TRUE)
+
+mean(guess$gesellschaft, na.rm = TRUE)
+sd(guess$gesellschaft, na.rm = TRUE)
+
+mean(guess$umfeldreaktion, na.rm = TRUE)
+sd(guess$umfeldreaktion, na.rm = TRUE)
+
+#DRITTER SCHRITT: Häufigkeit + Prozentangaben berechnen Variable "Geschlecht"
+#Zuerst Neue Variable mit Labels erstellen
+guess$geschlecht <- factor(
+  guess$Q7.2,
+  levels = c(0, 1),
+  labels = c("Frau", "Mann")
+)
+table(guess$geschlecht) #Häufigkeit anzeigen
+prop.table(table(guess$geschlecht)) * 100  #Prozentangaben berechnen
+
--- 04_Gruppenvergleiche.R
+++ 04_Gruppenvergleiche.R
@@ -2,8 +2,10 @@ library(openxlsx)
 guess <- read.xlsx("guess.xlsx", sheet = 1)
 
 guess_clean <- subset(guess, Q7.2 %in% c(0, 1))
+
 t.test(Q2.1_1 ~ Q7.2, data = guess_clean)
 
 t.test(Q2.1_2 ~ Q7.2, data = guess_clean)
 
 wilcox.test(Q2.1_1 ~ Q7.2, data = guess_clean)
+
